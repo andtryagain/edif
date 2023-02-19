d9ac8b39c78588221cd5f3c5c6734d7a6c188f5f
andtryagain <iliabondarev@protonmail.com>
2023-02-19 14:09:34 +0400

    port fix

--- run_docker.sh
+++ run_docker.sh
@@ -3,4 +3,4 @@
 docker image build -t flask_docker .
 # docker image build -t flask_docker . --build-arg INTERNAL_PORT=3000 EXTERNAL_PORT=3000
 
-docker run -p 3000:3000 -d flask_docker
+docker run -p 3000:433 -d flask_docker
